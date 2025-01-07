b93414af8b667f75e75aad0623c38c78476d9b01
KiSoo Song <idearbox@hotmail.com>
2025-01-07 11:19:21 +0900

    초기추가

---
+++ dotnet/samples/DotNetFX48/Program.cs
@@ -0,0 +1,20 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using OpenAI;
+using OpenAI.RealtimeConversation;
+using System.ClientModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFX48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
