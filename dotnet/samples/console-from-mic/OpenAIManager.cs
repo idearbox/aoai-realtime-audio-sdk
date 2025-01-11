f2d32d7b1de2e8c31ffd718aeb93a6f070b2fb46
KiSoo Song <idearbox@hotmail.com>
2025-01-11 14:25:03 +0900

    작업중

--- dotnet/samples/console-from-mic/OpenAIManager.cs
+++ dotnet/samples/console-from-mic/OpenAIManager.cs
@@ -16,22 +16,22 @@ using System.Linq;
 
 namespace console_with_mic
 {
-    public class OpenAIManager
+    public class OpenSGManager
     {
-        private static RealtimeConversationClient realtimeClient;
-        private static ChatClient chatClient;
-        private static ConversationFunctionTool m_finishConversationTool;
-        private static ConversationFunctionTool m_getAGVStateTool;
-        private static ConversationFunctionTool m_searchTool;
-        private static SpeakerOutput speakerOutput;
+        private RealtimeConversationClient m_realtimeClient;
+        private ChatClient m_chatClient;
+        private ConversationFunctionTool m_finishConversationTool;
+        private ConversationFunctionTool m_getAGVStateTool;
+        private ConversationFunctionTool m_searchTool;
+        private SpeakerOutput speakerOutput;
 
-        public async void RunOpenSGAgent()
+        public async Task RunAIAgent()
         {
             // First, we create a client according to configured environment variables (see end of file) and then start
             // a new conversation session.
             initClient();
 
-            using RealtimeConversationSession session = await realtimeClient.StartConversationSessionAsync();
+            using RealtimeConversationSession session = await m_realtimeClient.StartConversationSessionAsync();
             // Set the system message to guide the AI's behavior
             var contentItems = new List<ConversationContentPart>
            {
@@ -125,7 +125,173 @@ namespace console_with_mic
                 microphoneInput.StartRecording();
                 session.SendInputAudio(microphoneInput);
             }
-            Console.WriteLine("app end...");
+        }
+
+
+        private async Task ProcessResponse(RealtimeConversationSession session)
+        {
+            await foreach (ConversationUpdate update in session.ReceiveUpdatesAsync())
+            {
+                // session.created is the very first command on a session and lets us know that connection was successful.
+                if (update is ConversationSessionStartedUpdate)
+                {
+                    Console.WriteLine($" <<< Connected: session started");
+
+                    ////// This is a good time to start capturing microphone input and sending audio to the service. The
+                    ////// input stream will be chunked and sent asynchronously, so we don't need to await anything in the
+                    ////// processing loop.
+                    ////_ = Task.Run(async () =>
+                    ////{
+
+                    ////    using MicrophoneAudioStream microphoneInput = MicrophoneAudioStream.GetInstance();
+                    ////    microphoneInput.StartRecording();
+                    ////    Console.WriteLine($" >>> Listening to microphone input");
+                    ////    Console.WriteLine($" >>> (Just tell the app you're done to finish)");
+                    ////    Console.WriteLine();
+
+
+                    ////    await session.SendInputAudioAsync(microphoneInput);
+                    ////});
+                }
+
+                // input_audio_buffer.speech_started tells us that the beginning of speech was detected in the input audio
+                // we're sending from the microphone.
+                if (update is ConversationInputSpeechStartedUpdate speechStartedUpdate)
+                {
+                    Console.WriteLine($" <<< Start of speech detected @ {speechStartedUpdate.AudioStartTime}");
+                    // Like any good listener, we can use the cue that the user started speaking as a hint that the app
+                    // should stop talking. Note that we could also track the playback position and truncate the response
+                    // item so that the model doesn't "remember things it didn't say" -- that's not demonstrated here.
+                    //speakerOutput.ClearPlayback();
+                }
+
+                // input_audio_buffer.speech_stopped tells us that the end of speech was detected in the input audio sent
+                // from the microphone. It'll automatically tell the model to start generating a response to reply back.
+                if (update is ConversationInputSpeechFinishedUpdate speechFinishedUpdate)
+                {
+                    Console.WriteLine($" <<< End of speech detected @ {speechFinishedUpdate.AudioEndTime}");
+                }
+
+                // conversation.item.input_audio_transcription.completed will only arrive if input transcription was
+                // configured for the session. It provides a written representation of what the user said, which can
+                // provide good feedback about what the model will use to respond.
+                if (update is ConversationInputTranscriptionFinishedUpdate transcriptionFinishedUpdate)
+                {
+                    Console.WriteLine($" >>> USER: {transcriptionFinishedUpdate.Transcript}");
+                }
+
+                // Item streaming delta updates provide a combined view into incremental item data including output
+                // the audio response transcript, function arguments, and audio data.
+                if (update is ConversationItemStreamingPartDeltaUpdate deltaUpdate)
+                {
+                    Console.Write(deltaUpdate.AudioTranscript);
+                    Console.Write(deltaUpdate.Text);
+                    if (deltaUpdate.AudioBytes != null)
+                        speakerOutput.EnqueueForPlayback(deltaUpdate.AudioBytes);
+                    //else
+                    //    Console.Write("x");
+                }
+
+                // response.output_item.done tells us that a model-generated item with streaming content is completed.
+                // That's a good signal to provide a visual break and perform final evaluation of tool calls.
+
+                if (update is ConversationItemStreamingFinishedUpdate itemFinishedUpdate)
+                {
+                    Console.WriteLine();
+                    if (itemFinishedUpdate.FunctionName == m_getAGVStateTool.Name)
+                    {
+                        Console.WriteLine($" <<< Get Agv State tool invoked -- get!");
+                        string r = ToolsManager.GetAGVState();
+                        ConversationItem functionOutputItem = ConversationItem.CreateFunctionCallOutput(itemFinishedUpdate.FunctionCallId, r);
+
+                        await session.AddItemAsync(functionOutputItem);
+                        //await session.StartResponseAsync();
+                    }
+
+                    if (itemFinishedUpdate.FunctionName == m_searchTool.Name)
+                    {
+                        Console.WriteLine($" <<< **Search tool invoked -- get!");
+                        string r = ToolsManager.DoSearch(itemFinishedUpdate.FunctionCallArguments, m_chatClient);
+                        Console.WriteLine($" <<< **Search result : {r}");
+                        ConversationItem functionOutputItem = ConversationItem.CreateFunctionCallOutput(itemFinishedUpdate.FunctionCallId, r);
+
+                        await session.AddItemAsync(functionOutputItem);
+                        //await session.StartResponseAsync();
+                    }
+
+                    if (itemFinishedUpdate.FunctionName == m_finishConversationTool.Name)
+                    {
+                        Console.WriteLine($" <<< Finish tool invoked -- ending conversation!");
+                        break;
+                    }
+                }
+
+                if (update is ConversationResponseFinishedUpdate turnFinishedUpdate)
+                {
+                    Console.WriteLine($"  -- Model turn generation finished. Status: {turnFinishedUpdate.Status}");
+
+                    // Here, if we processed tool calls in the course of the model turn, we finish the
+                    // client turn to resume model generation. The next model turn will reflect the tool
+                    // responses that were already provided.
+                    if (turnFinishedUpdate.CreatedItems.Any(item => item.FunctionName?.Length > 0))
+                    {
+                        Console.WriteLine($"  -- Ending client turn for pending tool responses");
+                        await session.StartResponseAsync();
+                    }
+                    else
+                    {
+                    }
+                }
+
+                // error commands, as the name implies, are raised when something goes wrong.
+                if (update is ConversationErrorUpdate errorUpdate)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine($" <<< ERROR: {errorUpdate.Message}");
+                    Console.WriteLine(errorUpdate.GetRawContent().ToString());
+                    //break;
+                }
+            }
+        }
+
+        private void initClient()
+        {
+            m_realtimeClient = GetRealTimeClient();
+
+            ChatCompletionOptions options = new ChatCompletionOptions();
+            m_chatClient = GetChatClient();
+        }
+
+        private RealtimeConversationClient GetRealTimeClient()
+        {
+            //string? aoaiEndpoint = "https://kisoo-m3xuw55t-eastus2.cognitiveservices.azure.com";// "https://kisoo-m3xuw55t-eastus2.openai.azure.com/";// Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+            string? aoaiEndpoint = "https://kisoo-m3xuw55t-eastus2.openai.azure.com/";// Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+            string? aoaiDeployment = "gpt-4o-realtime-preview";// Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT");
+            string? aoaiApiKey = "8fCEvgpHLemju8nyMq2SSEIa4mH1ZEpYznBT1RBgTCqj7YVQhvYcJQQJ99AKACHYHv6XJ3w3AAAAACOG4BAa";// Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY");
+
+            AzureOpenAIClient aoaiClient = new(new Uri(aoaiEndpoint), new ApiKeyCredential(aoaiApiKey));
+            return aoaiClient.GetRealtimeConversationClient(aoaiDeployment);
+        }
+        private ChatClient GetChatClient()
+        {
+            //string? aoaiEndpoint = "https://kisoo-m3xuw55t-eastus2.cognitiveservices.azure.com/openai/realtime?api-version=2024-10-01-preview&deployment=gpt-4o-realtime-preview";// Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+
+            string? aoaiEndpoint = "https://kisoo-m3xuw55t-eastus2.cognitiveservices.azure.com/";// Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+            string? aoaiDeployment = "gpt-4o";// Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT");
+            string? aoaiApiKey = "8fCEvgpHLemju8nyMq2SSEIa4mH1ZEpYznBT1RBgTCqj7YVQhvYcJQQJ99AKACHYHv6XJ3w3AAAAACOG4BAa";// Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY");
+
+
+
+
+
+
+            AzureOpenAIClient aoaiClient = new(new Uri(aoaiEndpoint), new ApiKeyCredential(aoaiApiKey));
+
+            ChatClient _chatClient = aoaiClient.GetChatClient(aoaiDeployment);
+
+
+            return _chatClient;
         }
     }
 
